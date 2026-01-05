4cc3fcdf29f885edd8da6816bcc1738cbefdd2b2
Dominik K. <dominik@rivo.gg>
2026-01-05 15:25:47 +0100

    feat: add waitlist

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://usenotra.com',
+  output: "server",
   integrations: [
     mdx(),
     sitemap({
@@ -18,4 +20,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel(),
 });
