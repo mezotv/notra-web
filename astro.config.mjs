3f2b23c4b581fc9a867c0b9d014fda5f9623c7b6
Dominik K. <dominik@rivo.gg>
2026-01-13 19:40:09 +0100

    fix: use correct url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://usenotra.com',
+  site: 'https://www.usenotra.com',
   output: "server",
   integrations: [
     mdx(),
